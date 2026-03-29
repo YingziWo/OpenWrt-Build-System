35df18b288e051361f2206f5816e708acb33a957
YingziWo <104121906+YingziWo@users.noreply.github.com>
2026-03-29 21:27:48 +0800

    Change package source URLs to op-packages
    Updated package source URLs from 'openwrt-packages' to 'op-packages' for various components.

--- customize/lede_x86_64.sh
+++ customize/lede_x86_64.sh
@@ -33,27 +33,36 @@ git_sparse_clone main https://github.com/kenzok8/small-package luci-theme-argone
 git_sparse_clone main https://github.com/kenzok8/small-package luci-app-argone-config
 
 #luci-app-store
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-store
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-lib-taskd
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-lib-xterm
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages taskd
+#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-store
+git_sparse_clone master https://github.com/kiddin9/op-packages luci-app-store
+#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-lib-taskd
+git_sparse_clone master https://github.com/kiddin9/op-packages luci-lib-taskd
+#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-lib-xterm
+git_sparse_clone master https://github.com/kiddin9/op-packages luci-lib-xterm
+#git_sparse_clone master https://github.com/kiddin9/openwrt-packages taskd
+git_sparse_clone master https://github.com/kiddin9/op-packages taskd
 #更换插件名称
 sed -i 's/("iStore"),/("软件仓库"),/g' package/linpc/luci-app-store/luasrc/controller/store.lua
 
 #adguardhome
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-adguardhome
+#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-adguardhome
+git_sparse_clone master https://github.com/kiddin9/op-packages luci-app-adguardhome
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages adguardhome
 
 #科学上网
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-openclash
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-passwall
+#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-openclash
+git_sparse_clone master https://github.com/kiddin9/op-packages luci-app-openclash
+#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-passwall #引用的kiddin9目录已经变化，由openwrt-packages改op-packages ，2026.3.29改动
+git_sparse_clone master https://github.com/kiddin9/op-packages luci-app-passwall
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-ssr-plus
 #更换插件名称
 #sed -i 's/ShadowSocksR Plus+/软件插件/g' feeds/small8/luci-app-ssr-plus/luasrc/controller/shadowsocksr.lua
 
 #ddns-go
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages ddns-go
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-ddns-go
+#git_sparse_clone master https://github.com/kiddin9/openwrt-packages ddns-go
+git_sparse_clone master https://github.com/kiddin9/op-packages ddns-go
+#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-ddns-go
+git_sparse_clone master https://github.com/kiddin9/op-packages luci-app-ddns-go
 # rm -rf feeds/small8/ddns-go feeds/small8/luci-app-ddns-go
 # git clone --depth=1 https://github.com/sirpdboy/luci-app-ddns-go package/ddnsgo
 
@@ -78,39 +87,40 @@ git_sparse_clone v5 https://github.com/sbwml/luci-app-mosdns v2dat
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages zerotier
 
 #luci-app-autotimeset
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-autotimeset
+#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-autotimeset
+git_sparse_clone master https://github.com/kiddin9/op-packages luci-app-autotimeset
 
 
-########依赖包########
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages brook
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages chinadns-ng
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages dns2socks
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages dns2tcp
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages gn
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages hysteria
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages ipt2socks
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages microsocks
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages naiveproxy
+########依赖包########26.3.29 改openwrt-packages ---》》》 op-packages
+git_sparse_clone master https://github.com/kiddin9/op-packages brook
+git_sparse_clone master https://github.com/kiddin9/op-packages chinadns-ng
+git_sparse_clone master https://github.com/kiddin9/op-packages dns2socks
+git_sparse_clone master https://github.com/kiddin9/op-packages dns2tcp
+git_sparse_clone master https://github.com/kiddin9/op-packages gn
+git_sparse_clone master https://github.com/kiddin9/op-packages hysteria
+git_sparse_clone master https://github.com/kiddin9/op-packages ipt2socks
+git_sparse_clone master https://github.com/kiddin9/op-packages microsocks
+git_sparse_clone master https://github.com/kiddin9/op-packages naiveproxy
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages pdnsd-alt
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages shadowsocksr-libev
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages shadowsocks-rust
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages simple-obfs
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages sing-box
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages ssocks
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages tcping
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages trojan
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages trojan-go
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages trojan-plus
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages tuic-client
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages v2ray-core
+git_sparse_clone master https://github.com/kiddin9/op-packages shadowsocksr-libev
+git_sparse_clone master https://github.com/kiddin9/op-packages shadowsocks-rust
+git_sparse_clone master https://github.com/kiddin9/op-packages simple-obfs
+git_sparse_clone master https://github.com/kiddin9/op-packages sing-box
+git_sparse_clone master https://github.com/kiddin9/op-packages ssocks
+git_sparse_clone master https://github.com/kiddin9/op-packages tcping
+git_sparse_clone master https://github.com/kiddin9/op-packages trojan
+git_sparse_clone master https://github.com/kiddin9/op-packages trojan-go
+git_sparse_clone master https://github.com/kiddin9/op-packages trojan-plus
+git_sparse_clone master https://github.com/kiddin9/op-packages tuic-client
+git_sparse_clone master https://github.com/kiddin9/op-packages v2ray-core
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages v2ray-geodata
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages v2ray-plugin
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages xray-core
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages xray-plugin
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages lua-neturl
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages redsocks2
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages shadow-tls
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages lua-maxminddb
+git_sparse_clone master https://github.com/kiddin9/op-packages v2ray-plugin
+git_sparse_clone master https://github.com/kiddin9/op-packages xray-core
+git_sparse_clone master https://github.com/kiddin9/op-packages xray-plugin
+git_sparse_clone master https://github.com/kiddin9/op-packages lua-neturl
+git_sparse_clone master https://github.com/kiddin9/op-packages redsocks2
+git_sparse_clone master https://github.com/kiddin9/op-packages shadow-tls
+git_sparse_clone master https://github.com/kiddin9/op-packages lua-maxminddb
 
 ##########################################其他设置##########################################
 
